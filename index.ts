2337f7ca30b2ad4bdde61ee5c4673ce9ce9aa29b
Andrey Vyalkov <vyalkov.a@gmail.com>
2023-04-09 21:16:34 +0300

    add indext.ts

---
+++ index.ts
@@ -0,0 +1,5 @@
+// MIT License
+// Copyright (c) 2023 Andrey Vyalkov vyalkov.a@gmail.com
+// https://github.com/Andrey153/json-auto-form
+
+export { AutoForm6 as JSONAutoForm } from "./src/AutoForm6/AutoForm6";
